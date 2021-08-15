41438ac76e81fcbbf794058214624a25a606f444
Pramod Lawate <pramod.lawate@btl.com>
2021-08-15 16:51:19 +0100

    Added fake data to redis.

--- RedisApp.Api/Controllers/RedisController.cs
+++ RedisApp.Api/Controllers/RedisController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RedisApp.Api.Controllers
 {
@@ -17,6 +18,7 @@ namespace RedisApp.Api.Controllers
         public RedisController(IRedisCacheClient redisCcheClient)
         {
             _redisCcheClient = redisCcheClient;
+
         }
 
         [HttpGet("redis/{key}")]
@@ -26,9 +28,42 @@ namespace RedisApp.Api.Controllers
         }
 
         [HttpPost("redis/AddStudent")]
-        public async Task PostKeys([FromBody] string key, Student student)
+        public async Task PostKey([FromBody] string key, Student student)
         {
             await _redisCcheClient.GetDbFromConfiguration().AddAsync<Student>(key, student);
+        }
+
+        [HttpPost("redis/AddStudents")]
+        public async Task PostMultipleKeys([FromBody] string key, Student student)
+        {
+
+            List<Tuple<string, Student>> studentlist = new List<Tuple<string, Student>>();
+            studentlist.Add(new Tuple<string, Student>(key, student));
+            await _redisCcheClient.GetDbFromConfiguration().AddAllAsync<Student>(studentlist);
+        }
+
+        [HttpGet("FakeData")]
+        public async Task<List<Tuple<string, Student>>> InsertFakeData()
+        {
+            List<Tuple<string, Student>> fakedata = ReadFakeData();
+            await _redisCcheClient.GetDbFromConfiguration().AddAllAsync<Student>(fakedata);
+            return fakedata;
+        }
+
+        private List<Tuple<string, Student>> ReadFakeData()
+        {
+            var folderDetails = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\{"FakeData\\Fake.json"}");
+            var JSON = System.IO.File.ReadAllText(folderDetails);
+            var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Student>>(JSON);
+
+            List<Tuple<string, Student>> tuple = new List<Tuple<string, Student>>();
+            int counter = 0;
+            foreach (var item in jsonObj)
+            {
+                counter = counter + 1;
+                tuple.Add(new Tuple<string, Student>("key" + counter.ToString(), (Student)item));
+            }
+            return tuple;
         }
     }
 }
